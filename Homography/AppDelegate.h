643ce1426135c744b37f52fdf43c945b2c7b5e7b
Kenichi Inoue <inoccu@gmail.com>
2014-02-24 10:56:47 +0900

    Initial Commit

---
+++ Homography/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Homography
+//
+//  Created by 井上 研一 on 2014/02/24.
+//  Copyright (c) 2014年 Artisan Edge LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homography/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Homography
+//
+//  Created by 井上 研一 on 2014/02/24.
+//  Copyright (c) 2014年 Artisan Edge LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
